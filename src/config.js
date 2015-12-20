b466010266b5b2cfe1b9986f086dcaf309af07c8
Miro Jelicic <mjelicic@ebay.com>
2015-12-20 18:47:52 -0500

    updated config

--- src/config.js
+++ src/config.js
@@ -13,24 +13,17 @@ module.exports = Object.assign({
   host: process.env.HOST || 'localhost',
   port: process.env.PORT,
   app: {
-    title: 'React Redux Example',
-    description: 'All the modern best practices in one example.',
+    title: 'Adriatic House',
+    description: 'Looking for a perfect vacation? Stay at the Adriatic House and rest like never before!',
     meta: {
       charSet: 'utf-8',
       property: {
-        'og:site_name': 'React Redux Example',
-        'og:image': 'https://react-redux.herokuapp.com/logo.jpg',
+        'og:site_name': 'Adriatic House',
+        'og:image': 'http://adriatic.house/images/pool.jpg',
         'og:locale': 'en_US',
-        'og:title': 'React Redux Example',
-        'og:description': 'All the modern best practices in one example.',
-        'twitter:card': 'summary',
-        'twitter:site': '@erikras',
-        'twitter:creator': '@erikras',
-        'twitter:title': 'React Redux Example',
-        'twitter:description': 'All the modern best practices in one example.',
-        'twitter:image': 'https://react-redux.herokuapp.com/logo.jpg',
-        'twitter:image:width': '200',
-        'twitter:image:height': '200'
+        'og:title': 'Adriatic House',
+        'og:description': 'Looking for a perfect vacation? Stay at the Adriatic House and rest like never before!',
+        'twitter:card': 'summary'
       }
     }
   }
