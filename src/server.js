3a9d6dbadddbd84f899f04fea7bc6e18ec8333b3
Miro Jelicic <mjelicic@ebay.com>
2015-12-20 18:52:29 -0500

    cleaned up modules

--- src/server.js
+++ src/server.js
@@ -81,7 +81,6 @@ if (config.port) {
     if (err) {
       console.error(err);
     }
-    console.info('----\n==> ✅  %s is running, talking to API server on %s.', config.app.title, config.apiPort);
     console.info('==> 💻  Open http://%s:%s in a browser to view the app.', config.host, config.port);
   });
 } else {
